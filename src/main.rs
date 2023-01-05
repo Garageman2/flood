d944e362413a7cc1891eb9e169e4fbcab8f26793
Garageman <bbrantingham@gmail.com>
2023-01-05 14:56:03 -0800

    cleaned up

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 extern crate gif;
 extern crate image;
-extern crate termcolor;
 extern crate regex;
 use image::{Rgb,RgbImage,DynamicImage,io::Reader as ImageReader};
 use std::collections::{HashMap, VecDeque};
 use std::{thread,time};
-use std::io::Write;
-use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 use regex::Regex;
 
 fn gen_palette(img: &RgbImage)->HashMap<[u8;3],(u32,u32)>
@@ -19,19 +16,16 @@ fn gen_palette(img: &RgbImage)->HashMap<[u8;3],(u32,u32)>
     return cache;
 }
 
-fn col_dist(col:Rgb<u8>, new_col:Rgb<u8>) ->f32
-{
-    return ((col[0] as f32 - new_col[0] as f32).powf(2.0) +
-                            (col[1] as f32 - new_col[1] as f32).powf(2.0) +
-                            (col[2] as f32 - new_col[2] as f32).powf(2.0)).sqrt();
-
-}
+// fn col_dist(col:Rgb<u8>, new_col:Rgb<u8>) ->f32
+// {
+//     return ((col[0] as f32 - new_col[0] as f32).powf(2.0) +
+//                             (col[1] as f32 - new_col[1] as f32).powf(2.0) +
+//                             (col[2] as f32 - new_col[2] as f32).powf(2.0)).sqrt();
+//
+// }
 
 fn flood( img: &mut RgbImage, col:Rgb<u8>, new_col:Rgb<u8>, seed:(u32,u32))
 {
-    //$ Change threshold for tightness
-    const MAXDIST: f32 = 442.0;
-    const DISTTHRESHOLD: f32 = 1.0;
 
     //? will need to check that neighbors are == col to add to queue. Is a set needed for already flooded then?
     //TODO: needs benchmark but hash map for visited may be faster than repeating sqrt
@@ -43,7 +37,6 @@ fn flood( img: &mut RgbImage, col:Rgb<u8>, new_col:Rgb<u8>, seed:(u32,u32))
 
     while !queue.is_empty()
     {
-        //! needs to clamp summed values to width height of img
         let pix = queue.pop_front().unwrap();
 
 
@@ -54,18 +47,13 @@ fn flood( img: &mut RgbImage, col:Rgb<u8>, new_col:Rgb<u8>, seed:(u32,u32))
                                         (pix.1 as i32 + o.1).clamp(0,img.height() as i32 -1) as u32);
             let old = *img.get_pixel(temp.0,temp.1);
             if old != col
-            // # col_dist(*img.get_pixel(temp.0,temp.1),col)/MAXDIST < DISTTHRESHOLD
             {
                 continue;
             }
-            //println!("hit");
-            &img.put_pixel(temp.0,temp.1,new_col);
+            let _ = &img.put_pixel(temp.0,temp.1,new_col);
             queue.push_back(temp);
 
         }
-
-        // println!("Queue length {} ", queue.len());
-        // thread::sleep(time::Duration::from_secs_f32(0.2));
 
 
     }
@@ -75,7 +63,7 @@ fn input_pos(x:u32, y:u32) -> (u32,u32)
 {
     let mut line:String = String::new();
     let mut position:Option<(u32,u32)> = Option::None;
-    while (position == None)
+    while position == None
     {
         println!("enter a position in the format x,y ");
         std::io::stdin().read_line(&mut line).unwrap();
@@ -111,8 +99,8 @@ fn input_pos(x:u32, y:u32) -> (u32,u32)
 fn input_col() -> [u8;3]
 {
     let mut line:String = String::new();
-    let mut col:Option<[u8;3]> = Option::None;
-    while (col == None)
+    let mut col:Option<[u8;3]> = None;
+    while col == None
     {
         println!("enter a position in the format R,G,B such as 255,255,255 ");
         std::io::stdin().read_line(&mut line).unwrap();
@@ -182,8 +170,6 @@ fn main()
     }
 
 
-    //TODO: print out colors.
-    let mut stdout = StandardStream::stdout(ColorChoice::Always);
 
     let in_img = ImageReader::open("res/Godot.png")
         .expect("File not found!")
